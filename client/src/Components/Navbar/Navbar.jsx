56eaac950c3f7c87cf0dca9ac065e0aa06f513a4
arjunn881 <arjunn881@gmail.com>
2023-08-26 22:37:54 +0530

    22:37 26/08

--- client/src/Components/Navbar/Navbar.jsx
+++ client/src/Components/Navbar/Navbar.jsx
@@ -1,6 +1,9 @@
 import React from 'react'
 import './Navbar.scss';
 import Logo from '../../Asset/Logo.png';
+import SearchIcon from '@mui/icons-material/Search';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import ArrowDropDownIcon from '@mui/icons-material/ArrowDropDown';
 
 export const Navbar = () => {
   return (
@@ -14,7 +17,18 @@ export const Navbar = () => {
                 <span>My List</span>
             </div>
             <div className="right">
-
+                <SearchIcon className='icon'/>
+                <span>KID</span>
+                <NotificationsIcon className='icon'/>
+                <img src="" alt="" />
+                <div className="profile">
+                <ArrowDropDownIcon className='icon'/>
+                <div className="options">
+                  <span>Settings</span>
+                  <span>Logout</span>
+                </div>
+                </div>
+                
             </div>
         </div>
     </div>
