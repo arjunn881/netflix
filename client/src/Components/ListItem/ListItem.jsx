bd186118d988a31aeb021907a10b15c6ac2833ef
arjunn881 <arjunn881@gmail.com>
2023-08-28 14:14:50 +0530

    28/08 14:14

--- client/src/Components/ListItem/ListItem.jsx
+++ client/src/Components/ListItem/ListItem.jsx
@@ -1,8 +1,26 @@
-import React from 'react'
-import './ListItem.scss';
+import React from "react";
+import "./ListItem.scss";
+import PlayArrowIcon from '@mui/icons-material/PlayArrow';
+import AddIcon from '@mui/icons-material/Add';
+import ThumbUpOutlinedIcon from '@mui/icons-material/ThumbUpOutlined';
+import ThumbDownAltOutlinedIcon from '@mui/icons-material/ThumbDownAltOutlined';
 
 export const ListItem = () => {
   return (
-    <div className='listItem'>ListItem</div>
-  )
-}
+    <div className="listItem">
+      <img
+        src="https://igimages.gumlet.io/telugu/gallery/movies/sir2021/sirmain.jpg?w=640&dpr=1.0"
+        alt=""
+      />
+
+      <div className="itemInfo">
+        <div className="icons">
+          <PlayArrowIcon/>
+          <AddIcon/>
+          <ThumbUpOutlinedIcon/>
+          <ThumbDownAltOutlinedIcon/>
+        </div>
+      </div>
+    </div>
+  );
+};
