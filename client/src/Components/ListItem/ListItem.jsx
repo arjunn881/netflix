a8ff5a5dd39ce90fa911b7bcdb9e478011ab562c
arjunn881 <arjunn881@gmail.com>
2023-08-28 13:16:14 +0530

    28/08 13:16

---
+++ client/src/Components/ListItem/ListItem.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import './ListItem.scss';
+
+export const ListItem = () => {
+  return (
+    <div className='listItem'>ListItem</div>
+  )
+}
--- client/src/Home/Home.jsx
+++ client/src/Home/Home.jsx
@@ -2,13 +2,15 @@ import React from 'react'
 import './Home.scss';
 import { Navbar } from '../Components/Navbar/Navbar';
 import { Featured } from '../Components/Featured/Featured';
-import { List } from '@mui/material';
+import { List } from '../Components/List/List';
+
 
 export const Home = () => {
   return (
     <div className='home'>
         <Navbar/>
         <Featured type="movie"/>
+        <List/>
         <List/>
     </div>
   )
