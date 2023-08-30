3de3432cb3a60d600c9e575149cda40edadc2605
arjunn881 <arjunn881@gmail.com>
2023-08-30 23:54:58 +0530

    30/08 23:54

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,28 +1,31 @@
-import express from 'express';
-import User from '../models/User.js';
+import express from "express";
+import User from "../models/User.js";
+import CryptoJS from "crypto-js";
 
 const router = express.Router();
 
 //Register
 
-router.post('/register', async(req,res)=>{
-    const newUser = new User({
-        username:req.body.username,
-        email:req.body.email,
-        password : req.body.password
-    });
+router.post("/register", async (req, res) => {
+  const newUser = new User({
+    username: req.body.username,
+    email: req.body.email,
+    password: CryptoJS.AES.encrypt(
+      req.body.password,
+      process.env.SECRET_KEY
+    ).toString(),
+  });
 
-    try {
+  try {
+    const user = await newUser.save();
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
 
-        const user = await newUser.save();
-        res.status(200).json(user);
-        
-    } catch (error) {
-        res.status(500).json(error)
-    }
-
-
-
-})
+router.get("/", (req, res) => {
+  res.json("working...");
+});
 
 export default router;
