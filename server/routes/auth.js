b27b6c5c31b8d0b679b10dc30eea319d9c1d8ce9
arjunn881 <arjunn881@gmail.com>
2023-08-30 23:35:29 +0530

    updated

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -5,15 +5,23 @@ const router = express.Router();
 
 //Register
 
-router.post('/register', (req,res)=>{
+router.post('/register', async(req,res)=>{
     const newUser = new User({
         username:req.body.username,
         email:req.body.email,
         password : req.body.password
     });
 
-    const user = newUser.save();
-    res.status(200).json(user);
+    try {
+
+        const user = await newUser.save();
+        res.status(200).json(user);
+        
+    } catch (error) {
+        res.status(500).json(error)
+    }
+
+
 
 })
 
