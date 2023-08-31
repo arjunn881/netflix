0d027411bb552da80c589165d11f58c6aa767480
arjunn881 <arjunn881@gmail.com>
2023-08-31 22:09:50 +0530

    31/08 22:09

--- server/index.js
+++ server/index.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 
 import dotenv from "dotenv";
 import authRouter from "./routes/auth.js";
+import userRouter from './routes/users.js';
 
 const app = express();
 
@@ -20,7 +21,8 @@ mongoose
 
 
 
-  app.use('/api/auth', authRouter)
+  app.use('/api/auth', authRouter);
+  app.use('/api/user', userRouter)
 
 app.listen(8000, () => {
   console.log("Backend Server is running....");
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -28,4 +28,32 @@ router.get("/", (req, res) => {
   res.json("working...");
 });
 
+
+//LOGIN
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({ email: req.body.email });
+    !user && res.status(401).json("Wrong password or username!");
+
+    const bytes = CryptoJS.AES.decrypt(user.password, process.env.SECRET_KEY);
+    const originalPassword = bytes.toString(CryptoJS.enc.Utf8);
+
+    originalPassword !== req.body.password &&
+      res.status(401).json("Wrong password or username!");
+
+    const accessToken = jwt.sign(
+      { id: user._id, isAdmin: user.isAdmin },
+      process.env.SECRET_KEY,
+      { expiresIn: "5d" }
+    );
+
+    const { password, ...info } = user._doc;
+
+    res.status(200).json({ ...info, accessToken });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+
 export default router;
