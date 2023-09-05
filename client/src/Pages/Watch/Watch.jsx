281c9690d7d3078ed584c00ceb097d61646c8705
arjunn881 <arjunn881@gmail.com>
2023-09-05 23:40:57 +0530

    updated

--- client/src/Pages/Watch/Watch.jsx
+++ client/src/Pages/Watch/Watch.jsx
@@ -1,19 +1,23 @@
 import React from 'react'
 import './Watch.scss';
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
-import { useLocation } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 export const Watch = () => {
   const location = useLocation();
   const movie = location.movie;
   return (
     <div className='watch'>
-        <div className="back">
+      <Link to='/'>
+
+      <div className="back">
             <ArrowBackIcon/>
             Home
         </div>
+      </Link>
 
-        <video className='video' autoPlay progress controls src={movie}/>
+
+        <video className='video' autoPlay progress controls src={movie.video}/>
     </div>
   )
 }
